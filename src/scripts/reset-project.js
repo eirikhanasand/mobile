b7366e8e561a400773b7a6966d744831d7132b6b
eirikhanasand <eirik.hanasand@gmail.com>
2024-10-04 15:37:24 +0200

    Changed indenting to 4

--- src/scripts/reset-project.js
+++ src/scripts/reset-project.js
@@ -17,57 +17,55 @@ const newAppDirPath = path.join(root, 'app')
 const indexContent = `import { Text, View } from "react-native"
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  )
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    )
 }
 `
 
 const layoutContent = `import { Stack } from "expo-router"
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  )
+    return (
+        <Stack>
+            <Stack.Screen name="index" />
+        </Stack>
+    )
 }
 `
 
 fs.rename(oldDirPath, newDirPath, (error) => {
-  if (error) {
-    return console.error(`Error renaming directory: ${error}`)
-  }
-  console.log('/app moved to /app-example.')
-
-  fs.mkdir(newAppDirPath, { recursive: true }, (error) => {
     if (error) {
-      return console.error(`Error creating new app directory: ${error}`)
+        return console.error(`Error renaming directory: ${error}`)
     }
-    console.log('New /app directory created.')
+    console.log('/app moved to /app-example.')
 
-    const indexPath = path.join(newAppDirPath, 'index.tsx')
-    fs.writeFile(indexPath, indexContent, (error) => {
-      if (error) {
-        return console.error(`Error creating index.tsx: ${error}`)
-      }
-      console.log('app/index.tsx created.')
-
-      const layoutPath = path.join(newAppDirPath, '_layout.tsx')
-      fs.writeFile(layoutPath, layoutContent, (error) => {
+    fs.mkdir(newAppDirPath, { recursive: true }, (error) => {
         if (error) {
-          return console.error(`Error creating _layout.tsx: ${error}`)
+            return console.error(`Error creating new app directory: ${error}`)
         }
-        console.log('app/_layout.tsx created.')
-      })
+        console.log('New /app directory created.')
+
+        const indexPath = path.join(newAppDirPath, 'index.tsx')
+        fs.writeFile(indexPath, indexContent, (error) => {
+            if (error) {
+                return console.error(`Error creating index.tsx: ${error}`)
+            }
+            console.log('app/index.tsx created.')
+
+            const layoutPath = path.join(newAppDirPath, '_layout.tsx')
+            fs.writeFile(layoutPath, layoutContent, (error) => {
+                if (error) {
+                    return console.error(`Error creating _layout.tsx: ${error}`)
+                }
+                console.log('app/_layout.tsx created.')
+            })
+        })
     })
-  })
 })
