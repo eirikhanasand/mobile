fd631f7e6356c88d35e81a0b8ec6b142d85fff12
eirikhanasand <eirik.hanasand@gmail.com>
2024-10-23 00:43:30 +0200

    API draft

--- api/src/questions.ts
+++ api/src/questions.ts
@@ -3,6 +3,18 @@ const Questions = [
         title_no: "Hva er dette?",
         title_en: "What is this?",
     },
+    {
+        title_no: "Hva er 2?",
+        title_en: "What is 2?",
+    },
+    {
+        title_no: "Hva er 3?",
+        title_en: "What is 2?",
+    },
+    {
+        title_no: "Hva er 4?",
+        title_en: "What is 4?",
+    },
 ]
 
 export default Questions
--- api/types.d.ts
+++ api/types.d.ts
@@ -9,3 +9,18 @@ type Game = {
     description_no: string
     description_en: string
 }
+
+type Lobby = {
+    players: string[]
+    // inlobby = Currently in lobby, waiting for host to start
+    // ingame = round in progress
+    // waiting = round was just completed, waiting for host to continue
+    status: "inlobby" | "ingame" | "waiting"
+    current?: number
+    questions?: Question[]
+}
+
+type Player = {
+    id: string
+    name: string
+}
