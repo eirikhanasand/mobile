f7d3e806202e6bd24032dcd8a94768862928a8ee
amjohnse <amjohnse@stud.ntnu.no>
2024-11-12 16:34:03 +0100

    Fixed bugs

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,20 +1,19 @@
 // Imports default configuration function from '@react-native/metro-config'
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config')
 
 // Imports the Expo Metro configuration function
-const { getDefaultConfig: getExpoDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig: getExpoDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('metro-config').MetroConfig} */
 // Merges the configurations from both '@react-native/metro-config' and Expo
-const config = getExpoDefaultConfig(__dirname);
+const config = getExpoDefaultConfig(__dirname)
 
-// Enables css support
-config.resolver.sourceExts.push('css');
-
-// Add SVG support to the resolver
-const { resolver: { sourceExts, assetExts } } = getDefaultConfig.getDefaultValues();
-config.resolver.assetExts = assetExts.filter(ext => ext !== 'svg');
+// Enables css and svg support
+config.resolver.sourceExts.push('css')
 config.resolver.sourceExts.push('svg');
 
-// Export the merged configuration
-module.exports = config;
+// Exports the merged configuration
+module.exports = {
+    ...getDefaultConfig(__dirname),
+    ...config
+}
